7de9c57f695cbbf23362e81570a5f0fe81d5b6d8
杨操 <yangcao@boco.com.cn>
2018-04-17 18:17:04 +0800

    杨操 <yangcao@boco.com.cn>

--- springcloud-commons/src/main/java/com/github/springcloud/commons/util/httpclient/HttpUtil.java
+++ springcloud-commons/src/main/java/com/github/springcloud/commons/util/httpclient/HttpUtil.java
@@ -11,6 +11,8 @@ import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,12 +39,15 @@ import com.alibaba.fastjson.JSON;
 import com.github.springcloud.commons.model.Email;
 
 public class HttpUtil {
-	/**
-	 * URLConnection 原始方式发送Http请求
-	 */
+	
 	private static Logger logger = LoggerFactory.getLogger(HttpUtil.class);
 	public static final String REQUEST_METHOD_POST = "POST";
 	public static final String REQUEST_METHOD_GET = "GET";
+	public static final String REQUEST_CHARSET_UTF8 = "UTF-8";
+	public static final String CONTENT_TYPE_MULTIPART = "multipart/form-data";
+	public static final String CONTENT_TYPE_DEFAULT = "application/x-www-form-urlencoded";
+	public static final String CONTENT_TYPE_JSON = "application/json";
+	public static final String CONTENT_TYPE_TEXT = "text/html";
 
 	public static boolean downLoadFile(String attachUrl, String localfilepath, String localFileName) {
 		try {
@@ -56,7 +61,6 @@ public class HttpUtil {
 			int size = 0;
 			InputStream is = urlconnection.getInputStream();
 			BufferedInputStream bufferedinputstream = new BufferedInputStream(is);
-			// long filelength = bufferedinputstream.available();
 			if (bufferedinputstream != null) {
 				fos = new FileOutputStream(localfilepath + File.separator + localFileName);
 				logger.debug("正在获取链接[" + url + "]的内容...\n将其保存为文件[" + localFileName + "]");
@@ -77,13 +81,18 @@ public class HttpUtil {
 	}
 
 	public static String request(String url, Map<String, String> nameValuePairs, String requestMethod) {
+		return request(url, nameValuePairs, requestMethod, null);
+	}
+	/**
+	 * 参数为 xxxx?xxx=xxx&xx=xxx
+	 */
+	public static String request(String url, Map<String, String> nameValuePairs, String requestMethod, 
+			String contentType) {
 		PrintWriter out = null;
 		BufferedReader in = null;
-		String result = "ERROR";
+		String result = "URL[" + url + "]请求失败";
 		try {
-			if (requestMethod == null
-					|| (!requestMethod.toUpperCase().equals(HttpUtil.REQUEST_METHOD_POST) && !requestMethod
-							.toUpperCase().equals(HttpUtil.REQUEST_METHOD_GET))) {
+			if (requestMethod == null||!requestMethod.toUpperCase().equals(HttpUtil.REQUEST_METHOD_POST)) {
 				requestMethod = HttpUtil.REQUEST_METHOD_GET;
 			} else {
 				requestMethod = HttpUtil.REQUEST_METHOD_POST;
@@ -92,7 +101,7 @@ public class HttpUtil {
 			if (nameValuePairs != null && !nameValuePairs.isEmpty()) {
 				Set<Entry<String, String>> nameValuePairSet = nameValuePairs.entrySet();
 				for (Entry<String, String> nameValue : nameValuePairSet) {
-					String nameValuePair = nameValue.getKey() + "=" + nameValue.getValue();
+					String nameValuePair = nameValue.getKey() + "=" + URLEncoder.encode(nameValue.getValue(), HttpUtil.REQUEST_CHARSET_UTF8);
 					if (reqParams == null) {
 						reqParams = nameValuePair;
 					} else {
@@ -106,10 +115,19 @@ public class HttpUtil {
 			URL realUrl = new URL(url);
 			HttpURLConnection conn = (HttpURLConnection) realUrl.openConnection();
 			conn.setRequestProperty("accept", "*/*");
-			conn.setRequestProperty("Content-Type", "application/octet-stream");
+			if (contentType == null) {
+				//可以不设置
+				if (HttpUtil.REQUEST_METHOD_POST.equals(requestMethod)) {
+					//POST 此种方式不可设置为JSON
+//					conn.setRequestProperty("Content-Type", HttpUtil.CONTENT_TYPE_JSON);
+				} else {
+					conn.setRequestProperty("Content-Type", HttpUtil.CONTENT_TYPE_DEFAULT);
+				}
+			} else {
+				conn.setRequestProperty("Content-Type", contentType);
+			}
 			conn.setRequestProperty("connection", "Keep-Alive");
 			conn.setRequestProperty("Charset", "UTF-8");
-			conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
 			conn.setConnectTimeout(10000);// 连接的超时时间
 			conn.setReadTimeout(5000);// 读数据的超时时间
 			// conn.setRequestProperty("Content-Length", 100);//设置请求消息头 可以设置多个
@@ -123,7 +141,7 @@ public class HttpUtil {
 			}
 			if (HttpUtil.REQUEST_METHOD_POST.equals(requestMethod)) {
 				out = new PrintWriter(conn.getOutputStream());
-				out.print(reqParams);
+				out.print(reqParams);//url参数部分
 				out.flush();
 			}
 
@@ -143,24 +161,130 @@ public class HttpUtil {
 			logger.debug("发送 HTTP请求出现异常！" + e.getMessage());
 			e.printStackTrace();
 		} finally {
-			try {
-				if (out != null) {
-					out.close();
-				}
-				if (in != null) {
-					in.close();
-				}
-			} catch (IOException e) {
-				logger.debug("发送 HTTP请求出现异常！" + e.getMessage());
-				e.printStackTrace();
-			}
+			close(out, in);
 		}
 		return result;
+	}
+	/**
+	 * POST对象 提交JSON对象
+	 */
+	public static <T> String submit(String url, T t) {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String result = "URL[" + url + "]请求失败";
+		try {
+			URL realUrl = new URL(url);
+			HttpURLConnection conn = (HttpURLConnection) realUrl.openConnection();
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("Content-Type", HttpUtil.CONTENT_TYPE_JSON);
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("Charset", "UTF-8");
+			conn.setConnectTimeout(10000);
+			conn.setReadTimeout(5000);
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
+			String body = JSON.toJSONString(t);
+			out = new PrintWriter(conn.getOutputStream());
+			out.print(body);
+			out.flush();
+			int responseCode = conn.getResponseCode();
+			if (responseCode == 200) {
+				in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				String line;
+				result = "";
+				while ((line = in.readLine()) != null) {
+					result += line;
+				}
+				if ("".equals(result)) {
+					result = "OK";
+				}
+			}
+			conn.disconnect();
+		} catch (Exception e) {
+			logger.debug("发送 HTTP请求出现异常！" + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			close(out, in);
+		}
+		return result;
+	}
+	/**
+	 * POST 提交不同类型请求参数 JSON+xxxx?xxx=xxx&xx=xxx
+	 */
+	public static <T> String submitMulti(String url, Map<String, String> nameValuePairs,T t) {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String result = "URL[" + url + "]请求失败";
+		try {
+			String reqParams = null;
+			if (nameValuePairs != null && !nameValuePairs.isEmpty()) {
+				Set<Entry<String, String>> nameValuePairSet = nameValuePairs.entrySet();
+				for (Entry<String, String> nameValue : nameValuePairSet) {
+					String nameValuePair = nameValue.getKey() + "=" + URLEncoder.encode(nameValue.getValue(), HttpUtil.REQUEST_CHARSET_UTF8);
+					if (reqParams == null) {
+						reqParams = nameValuePair;
+					} else {
+						reqParams = reqParams + "&" + nameValuePair;
+					}
+				}
+			}
+			if (reqParams != null) {
+				url = url + "?" + reqParams;
+			}
+			URL realUrl = new URL(url);
+			HttpURLConnection conn = (HttpURLConnection) realUrl.openConnection();
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("Content-Type", HttpUtil.CONTENT_TYPE_JSON);//类型必须设置为JSON
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("Charset", "UTF-8");
+			conn.setConnectTimeout(10000);
+			conn.setReadTimeout(5000);
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
+			String bodyData = JSON.toJSONString(t);
+			out = new PrintWriter(conn.getOutputStream());
+			out.print(bodyData);
+			out.flush();
+			int responseCode = conn.getResponseCode();
+			if (responseCode == 200) {
+				in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				String line;
+				result = "";
+				while ((line = in.readLine()) != null) {
+					result += line;
+				}
+				if ("".equals(result)) {
+					result = "OK";
+				}
+			}
+//			conn.disconnect();
+		} catch (Exception e) {
+			logger.debug("发送 HTTP请求出现异常！" + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			close(out, in);
+		}
+		return result;
+	}
+	
+	private static void close(PrintWriter out, BufferedReader in) {
+		try {
+			if (out != null) {
+				out.close();
+			}
+			if (in != null) {
+				in.close();
+			}
+
+		} catch (IOException e) {
+			logger.debug("发送 HTTP请求出现异常！" + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 	public static String post(String url, Map<String, String> nameValuePairs) throws IOException {
 		String resultErrorDesc = "URL[" + url + "]请求失败";
-		String result = "ERROR";
+		String result = "URL[" + url + "]请求失败";
 		CloseableHttpResponse response = null;
 		CloseableHttpClient httpClient = null;
 		try {
@@ -200,7 +324,7 @@ public class HttpUtil {
 
 	public static String get(String url, Map<String, String> nameValuePairs) {
 		String resultErrorDesc = "URL[" + url + "]请求失败";
-		String result = "ERROR";
+		String result = "URL[" + url + "]请求失败";
 		CloseableHttpResponse response = null;
 		CloseableHttpClient httpClient = null;
 		try {
@@ -239,21 +363,24 @@ public class HttpUtil {
 
 	public static void main(String[] args) throws IOException {
 		String ROOT_URL = "http://localhost:8762";
-		String result = "";
-		// result = HttpUtil.get(ROOT_URL
-		// + "/EmailController/getUserAndsendSystemEmail", null);
-		// System.out.println(result);
-
-		result = HttpUtil.request(ROOT_URL + "/EmailController/getUserAndsendSystemEmail", null, null);
+		String result = "测试开始......";
+		System.out.println(result);
 		Email email = new Email();
 		email.setCuid("123456");
 		email.setDestnation("610039525@qq.com");
 		Map<String, String> nameValuePairs = new HashMap<String, String>();
 		nameValuePairs.put("email", JSON.toJSONString(email));
-		result = HttpUtil.request(ROOT_URL + "/EmailController/sendPrizeEmail", nameValuePairs, "POST");
-		// result = HttpUtil.post(ROOT_URL
-		// + "/EmailController/sendPrizeEmail", nameValuePairs);
-		System.out.println(result);
-
+		//URL参数
+//		result = HttpUtil.request(ROOT_URL + "/EmailController/sendPrizeEmailStr", nameValuePairs, "GET");
+		result = HttpUtil.request(ROOT_URL + "/EmailController/sendPrizeEmailStr", nameValuePairs, "POST");
+		System.out.println("1=>"+result);		
+		//JSON对象
+//		result = HttpUtil.submit(ROOT_URL + "/EmailController/sendPrizeEmail", email);
+		System.out.println("2=>"+result);
+		//复合参数
+		Map<String, String> urlNameValuePairs = new HashMap<String, String>();
+		urlNameValuePairs.put("phone", "18502817833");
+//		result = HttpUtil.submitMulti(ROOT_URL + "/EmailController/sendPrizeEmailMulti", urlNameValuePairs, email);
+		System.out.println("3=>"+result);
 	}
 }
